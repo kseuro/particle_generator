9267e2cdbe67c4a465bda74267939de6ade2f56a
Kai Stewart <kai.kharpertian@gmail.com>
2019-11-18 19:35:47 -0500

    added dynamic model allocation functionality

--- gan.py
+++ gan.py
@@ -7,7 +7,7 @@
 # Purpose: - This file provides the PyTorch versions of the generator and
 #            discriminator network models as multi-layer perceptrons
 #          - Model architecture is decided at run-time based on the
-#            provided command line arguments,
+#            provided command line arguments and is dynamically allocated
 ################################################################################
 
 # Torch Imports
@@ -35,48 +35,54 @@ class Print(nn.Module):
         print(x.shape)
         return x
 
+def FullyConnected(in_f, out_f):
+    '''
+        Fully connected layers used by both G and D
+    '''
+    return nn.Sequential(
+        nn.Linear(in_f, out_f),
+        nn.LeakyReLU(0.2)
+    )
+
+def G_out(in_f, out_f):
+    '''
+        Output layer of the generator model
+    '''
+    return nn.Sequential(
+        nn.Linear(in_f, out_f),
+        nn.Tanh()
+    )
+
+def D_out(in_f, out_f):
+    '''
+        Output layer of the discriminator model
+    '''
+    return nn.Sequential(
+        nn.Linear(in_f, out_f),
+        nn.Sigmoid()
+    )
+
 class Generator(nn.Module):
     '''
         Generator model class
         Does: initializes a G model using the provided arch specs.
         Args: - z_dim (int): dimension of the input vector
-              - n_layers (int): number of layers in Generator model
-              - n_hidden (int): number of hidden units in each layer
+              - fc_sizes (list): list of layers sizes to add to model
+                - example: fc_sizes = [32, 64, 128] is a 3 layer model
+                           with sizes 32, 64, and 128. 
               - n_out (int): dimension of model output layer. Should correspond
                        to the square of the input image dimension
                        - (e.g. [512x512] = 262144)
-              - optimizer (string): string specifying G's optimizer (adam, sgd)
-              - optim_kwargs (dict): dict of optimizer parameters. See docs for
-                                     parameter details:
-                                     https://pytorch.org/docs/stable/optim.html
         Returns: Generator model
     '''
-    def __init__(self, z_dim, n_layers, n_hidden, n_out):
+    def __init__(self, z_dim, fc_sizes, n_out):
         super(Generator, self).__init__()
-        self.in_features  = z_dim
-        self.n_layers     = n_layers
-        self.n_hidden     = n_hidden
-        self.out_features = n_out
-        self.activation   = nn.LeakyReLU(0.2)
-        self.out_activ    = nn.Tanh()
-
-        # Prepare model
-        ## First layers
-        self.front = nn.Sequential(nn.Linear(self.in_features, self.n_hidden),
-                                             self.activation)
-
-        ## Middle layers
-        self.layers = []
-        for _ in range(self.n_layers - 2):
-            self.layers += nn.Sequential(nn.Linear(self.n_hidden, self.n_hidden)
-                                                  ,self.activation)
-
-        ## Convert self.layers to ModuleList so that it registers properly
-        self.layers = nn.ModuleList([nn.ModuleList(layer) for layer in self.layers])
-
+        # Set size of input layer and unpack middle layer sizes as list
+        self.fc_sizes = [z_dim, *fc_sizes]
+        self.fc = nn.Sequential(*[FullyConnected(in_f, out_f)
+                                  for in_f, out_f in zip(self.fc_sizes, self.fc_sizes[1:])])
         ## Output layer
-        self.out = nn.Sequential(nn.Linear(self.n_hidden, self.out_features),
-                                           self.out_activ)
+        self.out = G_out(self.fc_sizes[-1], n_out)
 
     # Initialize the weights
     def weights_init(self):
@@ -92,11 +98,7 @@ class Generator(nn.Module):
                 m.bias.data.zero()
 
     def forward(self, x):
-        x = self.front(x)
-        # Loop over dynamically allocated layers
-        for _ , layer in enumerate(self.layers):
-            x = layer(x)
-        return self.out(x)
+        return self.out(self.fc(x))
 
 class Discriminator(nn.Module):
     '''
@@ -104,42 +106,17 @@ class Discriminator(nn.Module):
         Does: initializes a D model using the provided arch specs.
         Args: - in_features (int): dimension of the input layer, calculated based
                                    on the dimension of the input data images
-              - n_layers (int): number of layers in Discriminator model
-              - n_hidden (int): number of hidden units in each layer
-              - optimizer (string): string specifying G's optimizer (adam, sgd)
-              - optim_kwargs (dict): dict of optimizer parameters. See docs for
-                                     parameter details:
-                                     https://pytorch.org/docs/stable/optim.html
+              - fc_sizes (list): list of layers sizes to add to model
+                - example: fc_sizes = [32, 64, 128] is a 3 layer model
+                           with sizes 32, 64, and 128. 
         Returns: Probability of model input belonging to real image space
     '''
-    def __init__(self, in_features, n_layers, n_hidden):
+    def __init__(self, in_features, fc_sizes):
         super(Discriminator, self).__init__()
-        self.in_features  = in_features
-        self.n_layers     = n_layers
-        self.n_hidden     = n_hidden
-        self.activation   = nn.LeakyReLU(0.2)
-        self.dropout      = nn.Dropout(0.3)
-        self.out_features = 1
-        self.out_activ    = nn.Sigmoid()
-
-        # Prepare model
-        ## First layer
-        self.front = nn.Sequential(nn.Linear(self.in_features, self.n_hidden),
-                                             self.activation)
-
-        ## Middle layers
-        self.layers = []
-        for _ in range(self.n_layers - 2):
-            self.layers += nn.Sequential(nn.Linear(self.n_hidden, self.n_hidden)
-                                                  ,self.activation)
-
-        ## Convert self.layers to ModuleList so that it registers properly
-        self.layers = nn.ModuleList([nn.ModuleList(layer)
-                                     for layer in self.layers])
-
-        ## Output layer
-        self.out = nn.Sequential(nn.Linear(self.n_hidden, self.out_features),
-                                           self.out_activ)
+        self.fc_sizes = [in_features, *fc_sizes]
+        self.fc = nn.Sequential(*[FullyConnected(in_f, out_f)
+                                  for in_f, out_f in zip(self.fc_sizes, self.fc_sizes[1:])])
+        self.out = D_out(self.fc_sizes[-1], 1)
 
     # Initialize the weights
     def weights_init(self):
@@ -155,8 +132,4 @@ class Discriminator(nn.Module):
                 m.bias.data.zero()
 
     def forward(self, x):
-        x = self.front(x)
-        # Loop over dynamically allocated layers
-        for _ , layer in enumerate(self.layers):
-            x = layer(x)
-        return self.out(x)
+        return self.out(self.fc(x))
