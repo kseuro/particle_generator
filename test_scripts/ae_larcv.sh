86e5ce8c686b686805ce62ab5b94112eb43d2714
Kai <Kai.Kharpertian@gmail.com>
2020-01-09 14:18:38 -0500

    still cant ignore the AE test script...

--- test_scripts/ae_larcv.sh
+++ test_scripts/ae_larcv.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=1,2 python train.py \
+CUDA_VISIBLE_DEVICES=0,1,2 python train.py \
 --gpu 1 \
 --dataset 64 \
---batch_size 256 \
---num_epochs 15000 \
+--batch_size 128 \
+--num_epochs 7500 \
 --sample_size 16 \
 --shuffle True \
 --drop_last True \
 --num_workers 8 \
 --model ae \
 --n_layers 4 \
---l_dim 20 \
+--l_dim 256 \
 --ae_lr 1e-4 \
 --ae_opt adam \
 --beta 0.5 \
