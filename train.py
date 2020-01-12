5ef8642cedeea0defcd94a2f0f4ed0802ce2a180
Kai <Kai.Kharpertian@gmail.com>
2020-01-11 21:12:50 -0500

    forcing checkpoint savinging between 600 -- 1000 epochs

--- train.py
+++ train.py
@@ -94,15 +94,17 @@ def train(config):
         # Check losses starting after 600 epochs and determine if
         # the current model is the best model state
         if (epoch >= 600) and (epoch % 50 == 0):
-            for key in best_stat:
-                if best is None:
-                    best = best_stat[key]
-                    checkpoint = utils.get_checkpoint(epoch, kwargs, config)
-                    utils.save_checkpoint(checkpoint, config)
-                if round(best_stat[key], 5) < round(best, 5):
-                    best = best_stat[key]
-                    checkpoint = utils.get_checkpoint(epoch, kwargs, config)
-                    utils.save_checkpoint(checkpoint, config)
+            checkpoint = utils.get_checkpoint(epoch, kwargs, config)
+            utils.save_checkpoint(checkpoint, config)
+           # for key in best_stat:
+           #     if best is None:
+           #         best = best_stat[key]
+           #         checkpoint = utils.get_checkpoint(epoch, kwargs, config)
+           #         utils.save_checkpoint(checkpoint, config)
+           #     if round(best_stat[key], 5) < round(best, 5):
+           #         best = best_stat[key]
+           #         checkpoint = utils.get_checkpoint(epoch, kwargs, config)
+           #         utils.save_checkpoint(checkpoint, config)
 
     # Save training history and experiment config for evaluation and deploy
     utils.save_train_hist(history, best_stat, times, config)
