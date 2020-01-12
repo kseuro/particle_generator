4457e82c916ab75a775dd812b1eab0e6a93daaa3
Kai <Kai.Kharpertian@gmail.com>
2020-01-11 20:52:26 -0500

    added notebook for analyzing the delta_MSE of a trained AutoEncoder model

--- test_scripts/ae_larcv.sh
+++ test_scripts/ae_larcv.sh
@@ -3,14 +3,14 @@ CUDA_VISIBLE_DEVICES=1,2 python train.py \
 --gpu 1 \
 --dataset 64 \
 --batch_size 256 \
---num_epochs 15000 \
+--num_epochs 1000 \
 --sample_size 16 \
 --shuffle True \
 --drop_last True \
 --num_workers 8 \
 --model ae \
 --n_layers 4 \
---l_dim 20 \
+--l_dim 256 \
 --ae_lr 1e-4 \
 --ae_opt adam \
 --beta 0.5 \
