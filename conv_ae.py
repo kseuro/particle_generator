e9e12e7a522e94b3bbb6b72d4f8f0d5162ee01dd
Kai Stewart <Kai.Kharpertian@gmail.com>
2020-01-17 18:20:08 -0500

    Module.__init__() call

--- conv_ae.py
+++ conv_ae.py
@@ -48,7 +48,8 @@ class ConvAutoEncoder(nn.Module):
         This model assumes the use of 1-channel images. If using 3-channels,
         modify each instance of [1] to [3].
     '''
-    def __init__(self, enc_depth, dec_depth):      # Examples:
+    def __init__(self, enc_depth, dec_depth):
+        super().__init__()                         # Examples:
         self.enc_features = [1] + enc_depth        # [1, 4, 8, 16, 32]
         self.dec_features = dec_depth + [1]        # [32, 16, 8, 4, 1]
         self.encoder = ConvEncoder(self.enc_features)
