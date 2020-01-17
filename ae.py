3ff71dbf7ab20772b8df6962d51ef54407e0c18a
Kai Stewart <Kai.Kharpertian@gmail.com>
2020-01-17 17:19:32 -0500

    Added convolutional AutoEncoder class -- moved AE layer functions to separate file

--- ae.py
+++ ae.py
@@ -17,21 +17,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision
 
-def enc_block(in_f, out_f):
-    '''
-        Using LeakyReLU in the encoder portion generates much 'cleaner'
-        looking digits during MNIST PoC.
-    '''
-    return nn.Sequential(
-        nn.Linear(in_f, out_f),
-        nn.LeakyReLU(0.2)
-    )
-
-def dec_block(in_f, out_f):
-    return nn.Sequential(
-        nn.Linear(in_f, out_f),
-        nn.LeakyReLU(0.2)
-    )
+from ae_layers import *
 
 class Encoder(nn.Module):
     def __init__(self, enc_sizes, l_dim):
@@ -80,6 +66,56 @@ class AutoEncoder(nn.Module):
     def weights_init(self):
         self.encoder.weights_init()
         self.decoder.weights_init()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+class ConvEncoder(nn.Module):
+    '''
+        Convolutional AutoEncoder - Encoder branch
+        args: depth (int list): list of integer feature depth sizes
+                                that define the output volume of the
+                                individual convolution operations.
+                                The depth list is computed by dividing
+                                the desired depth by the number of layers.
+                                Ex: D = 32, n_layers = 4, depth = [4, 8, 16, 32]
+                                Adding the initial channel: [1, 4, 8, 16, 32]
+    '''
+    def __init__(self, depth):
+        super().__init__()
+        self.conv_blocks = nn.Sequential(*[conv_block(in_f, out_f) for in_f, out_f
+                                           in zip(depth, depth[1:])])
+    def forward(self, x):
+        return self.conv_blocks(x)
+
+class ConvDecoder(nn.Module):
+    '''
+        Convolutional AutoEncoder - Decoder branch
+        args: depth (int list): list of integer feature depth sizes
+                                that define the output volume of the
+                                individual transposed convolution operations.
+                                See ConvEncoder for example of list, which is
+                                reversed in the Decoder model.
+    '''
+    def __init__(self, depth):
+        super().__init__()
+        self.deconv_blocks = nn.Sequential(*[deconv_blocks(in_f, out_f) for in_f, out_f
+                                              in zip(depth, depth[1:])])
+        self.activation = nn.Tanh()
+    def forward(self, x):
+        return self.activation(self.deconv_blocks(x))
+
+class ConvAutoEncoder(nn.Module):
+    '''
+        Convolutional AutoEncoder model
+    '''
+    def __init__(self, depth):                 # Examples:
+        self.enc_features = [1] + depth        # [1, 4, 8, 16, 32]
+        self.dec_features = depth[::-1] + [1]  # [32, 16, 8, 4, 1]
+        self.encoder = ConvEncoder(self.enc_features)
+        self.decoder = ConvDecoder(self.dec_features)
 
     def forward(self, x):
         x = self.encoder(x)
---
+++ ae_layers.py
@@ -0,0 +1,37 @@
+def enc_block(in_f, out_f):
+    '''
+        Using LeakyReLU in the encoder portion generates much 'cleaner'
+        looking digits during MNIST PoC.
+    '''
+    return nn.Sequential(
+        nn.Linear(in_f, out_f),
+        nn.LeakyReLU(0.2)
+    )
+
+def dec_block(in_f, out_f):
+    return nn.Sequential(
+        nn.Linear(in_f, out_f),
+        nn.LeakyReLU(0.2)
+    )
+
+def conv_block(in_f, out_f):
+    '''
+        Convolutional blocks increase the depth of the feature maps from
+        in_f -> out_f. The MaxPool2d funtion then reduces the feature
+        map dimension by a factor of 2.
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_f, out_f, 3, padding=1),
+        nn.LeakyReLU(0.2),
+        nn.MaxPool2d(2,2)
+    )
+
+def deconv_blocks(in_f, out_f):
+    '''
+        ConvTranspose blocks decrease the depth of the feature maps from
+        in_f -> out_f. This sizing pattern is the opposite of the Conv_Blocks.
+    '''
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_f, out_f, 2, stride=2),
+        nn.LeakyReLU(0.2)
+    )
